f38ffeac62b1dd341ccebc70825d4e461ff70177
sebastian461 <sebastiano-461@hotmail.com>
2024-01-03 16:19:36 -0500

    Uso de slice (reducer)

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -13,7 +13,13 @@ export const counterSlice = createSlice({
     increment: (state) => {
       state.counter += 1;
     },
+    decrement: (state) => {
+      state.counter -= 1;
+    },
+    incrementBy: (state, action) => {
+      state.counter += action.payload;
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementBy } = counterSlice.actions;
